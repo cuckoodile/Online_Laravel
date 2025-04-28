e7631087cd2dc7a38bdeee2265a8e4c278e66eee
Alxer_Sans <alxerdvno09@gmail.com>
2025-04-29 02:48:44 +0800

    fixed models

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -11,25 +11,8 @@ class Address extends Model
         'house_address',
         'region',
         'province',
-<<<<<<< HEAD
-        'district',
-        'city_municipality',
-        'barangay',
-<<<<<<< HEAD
-        'subdivision_or_village', // (if applicable) // add |sometimes|'
-        'street_number',
-        'street_name',
-        'unit_number',  // (if applicable) // add |sometimes|'
-=======
         'city',
-        'baranggay', 
->>>>>>> 69bff22 (Product Comments)
-=======
-        'subdivision_village',
-        'street',
-        'block_number',
-        'lot_number',  
->>>>>>> c88078fc465a0c6707a08714657eabbb89d86fbf
+        'baranggay',
         'zip_code',
     ];
 
