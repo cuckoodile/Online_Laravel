51ea2698f690a8a435e90a62de9b332cbb4e50b4
cuckoodile <lhourdeiansube7@gmail.com>
2025-05-05 12:41:48 +0800

    Added ERD drawio file

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -22,12 +22,12 @@ class Transaction extends Model
         "status_id",
         "address_id",
         "is_void"
-
     ];
 
     public function user() {
         return $this->belongsTo(User::class);
     }
+
     public function transaction_types() {
         return $this->belongsTo(TransactionType::class,"type_id","id");
     }
@@ -39,9 +39,11 @@ class Transaction extends Model
     public function transaction_payment_methods() {
         return $this->belongsTo(TransactionPaymentMethod::class,"payment_method_id","id");
     }
+
     public function products() {
         return $this->belongsToMany(Product::class)->withPivot('quantity','total_price');
     }
+    
     public function address() {
         return $this->belongsTo(Address::class);
     }
